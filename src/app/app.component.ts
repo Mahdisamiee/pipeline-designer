2d86b3d8b316a9d9b45f853849b2c5d7eff7a5d3
m_lind <md.samieenia@gmail.com>
2023-11-14 16:26:47 +0330

    clean code app and sideNav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { PipeLineComponent } from './pipe-line/pipe-line.component';
 import { LayoutComponent } from './components/layout/layout.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, PipeLineComponent, LayoutComponent],
+  imports: [CommonModule, RouterOutlet, LayoutComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
